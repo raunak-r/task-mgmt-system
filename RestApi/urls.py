556b447a88e78b90e7677fc004f9843f1b85a96d
Raunak Ritesh <raunak.ritesh@happay.in>
2018-09-14 10:54:17 +0530

    Code cleaned for transition to GITHUB

--- RestApi/urls.py
+++ RestApi/urls.py
@@ -2,11 +2,7 @@
 from django.conf.urls import include, url
 
 from views import ip
-from views import visionApiFile
-from views import visionApiScript
 
 urlpatterns = [
     url(r'^ip', ip),
-    url(r'^vision/file', visionApiFile),
-    url(r'^vision/script', visionApiScript),
 ]
